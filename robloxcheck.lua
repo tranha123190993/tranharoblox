e0695a551b506e5fc6f021a396b8215c08f2baa3
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-01 10:45:09 +0700

    Update robloxcheck.lua

--- robloxcheck.lua
+++ robloxcheck.lua
@@ -1,7 +1,7 @@
 local RunService = game:GetService("RunService")
 RunService:Set3dRenderingEnabled(false)
 setfpscap(15)
-getgenv().Key = "MARU-78T3-G54LS-1DH2-AHI95-NI09"
+getgenv().Key = "MARU-NC03-TVRRW-7ZFM-KBVH8-WRHR"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
