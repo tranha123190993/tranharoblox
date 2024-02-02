d2791163f4b145ece4dde94fbe86c139c4b1314f
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 10:47:30 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,16 +1,11 @@
 task.spawn(function()
--- Enabled Kaitun Scripts
-_G.KaitanMode = true
--- Candy Event
-_G.CandyConfigs = {
-    ExpX2 = true,
-    Trade500Fragment = false,
-    Trade200Fragment = false
-    WhiteScreen = true
-}
-_G.Key = "LuxuryV2_2udjkys9ekq0skk3ozzh"
-_G.DiscordId = "541501601504821277"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NightsTimeZ/RoyryX/main/Loader.lua"))()
+local RunService = game:GetService("RunService")
+RunService:Set3dRenderingEnabled(false)
+setfpscap(15)
+getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
+getgenv().id = "513996919622860832"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 end)
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chimnguu/ngu/master/bululachip.lua"))()
