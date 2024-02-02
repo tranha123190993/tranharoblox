781109c529135e4e3c00577155e9777f0480ea50
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 10:05:08 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,5 +1,24 @@
-repeat task.wait() until game:IsLoaded()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cangyeunhaudau/ditmemaychominhtriet/main/mankefarm.lua"))()
+if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+
+_G.Delay_Upload = 300
+_G.Bypass_Tp = true
+
+_G.Select_Fruit = {
+    ['Main'] = {'Dough-Dough'},
+    ['Select Fruit'] = {'Dark-Dark','Human-Human: Buddha','Sand-Sand','Magma-Magma'}
+}
+_G.Mastery_Farm = {
+    ['Melee'] = true,
+    ['Devil Fruit'] = true,
+    ['Sword'] = true,
+    ['Gun'] = true,
+}
+_G.Race_Evo = {
+    ['Level'] = {1,2,3},
+    ['Enabled'] = true
+}
+script_key="UPUXjqSkewmtoHSQzjwManHoPDEaPuAe";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chimnguu/ngu/master/bululachip.lua"))()
 end)
