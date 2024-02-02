b05564effc8d72f580e2a9ad829e64970f64ca44
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 08:21:05 +0700

    Update robloxcheck.lua

--- robloxcheck.lua
+++ robloxcheck.lua
@@ -1,25 +1,11 @@
 task.spawn(function()
-if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
-
-_G.Delay_Upload = 300 -- โชว์ข้อมูลใหม่ทุกๆ 300วิ
-_G.Bypass_Tp = true -- วาปฟาร์ม
-
-_G.Select_Fruit = {
-    ['Main'] = {'Dough-Dough'}, -- เลือกผลปีศาจหลัก
-    ['Select Fruit'] = {'Dark-Dark','Human-Human: Buddha','Sand-Sand','Magma-Magma'} -- เลือกผลปีศาจ
-}
-_G.Mastery_Farm = { -- ฟามมาสเตอรี่
-    ['Melee'] = true,
-    ['Devil Fruit'] = true,
-    ['Sword'] = true,
-    ['Gun'] = true,
-}
-_G.Race_Evo = {
-    ['Level'] = {1,2,3}, -- อีโวเผ่าจากขั้น0 ไปถึงขั้น3
-    ['Enabled'] = true
-}
-script_key="UPUXjqSkewmtoHSQzjwManHoPDEaPuAe";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
+local RunService = game:GetService("RunService")
+RunService:Set3dRenderingEnabled(false)
+setfpscap(15)
+getgenv().Key = "MARU-82J4-4YRAQ-VKBN-N8LP3-LU9S"
+getgenv().id = "622118435924475905"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 end)
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chimnguu/ngu/master/bululachip.lua"))()
