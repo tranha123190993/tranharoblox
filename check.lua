69bbcc0a359240526c361111e55bd858aad9e19b
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 10:54:50 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/afxcaxi/Royx/main/AfxcaxiRoyxMain"))()
-task.spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhtriettt/Free-Script/main/MTriet-Hub.lua"))()
+spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chimnguu/ngu/master/bululachip.lua"))()
 end)
