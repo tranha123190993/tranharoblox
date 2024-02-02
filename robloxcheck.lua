7952495470c635c709ae179fbdbe224441261ee1
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 19:36:45 +0700

    Update robloxcheck.lua

--- robloxcheck.lua
+++ robloxcheck.lua
@@ -1,6 +1,4 @@
 task.spawn(function()
-local RunService = game:GetService("RunService")
-RunService:Set3dRenderingEnabled(false)
 setfpscap(15)
 getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
 getgenv().id = "513996919622860832"
