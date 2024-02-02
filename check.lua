ef9071843ad02a76eb07a8c206bf0111c00b5bba
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 09:39:26 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,4 +1,4 @@
-spawn(function()
+task.spawn(function()
 local RunService = game:GetService("RunService")
 RunService:Set3dRenderingEnabled(false)
 setfpscap(15)
@@ -7,6 +7,6 @@ getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 end)
-spawn(function()
+task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chimnguu/ngu/master/bululachip.lua"))()
 end)
