d8e161647db85efec062a60b27569cb4f9912ce7
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 10:50:37 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,12 +1,4 @@
-task.spawn(function()
-local RunService = game:GetService("RunService")
-RunService:Set3dRenderingEnabled(false)
-setfpscap(15)
-getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
-getgenv().id = "513996919622860832"
-getgenv().Script_Mode = "Kaitun_Script"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/afxcaxi/Royx/main/AfxcaxiRoyxMain"))()
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chimnguu/ngu/master/bululachip.lua"))()
 end)
