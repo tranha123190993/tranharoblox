9027331811c92a2081a1cd16d1315d0c81f4ec6d
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 19:46:57 +0700

    Update robloxcheck.lua

--- robloxcheck.lua
+++ robloxcheck.lua
@@ -1,3 +1,8 @@
+setfpscap(15)
+getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
+getgenv().id = "513996919622860832"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chimnguu/ngu/master/bululachip.lua"))()
 end)
