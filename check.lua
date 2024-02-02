bb76e158fd82531817784ae81cb9755e964e36d9
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-02 17:02:11 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -2,7 +2,7 @@ spawn(function()
 local RunService = game:GetService("RunService")
 RunService:Set3dRenderingEnabled(false)
 setfpscap(15)
-getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
+getgenv().Key = "MARU-78T3-G54LS-1DH2-AHI95-NI09"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
