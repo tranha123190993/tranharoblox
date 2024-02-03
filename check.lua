f812d00f5795e0048f4841a4d78ec5ffa2c0fcbd
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-03 08:44:41 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,14 +1,14 @@
 spawn(function()
 local RunService = game:GetService("RunService")
 RunService:Set3dRenderingEnabled(false)
-setfpscap(15)
+setfpscap(20)
 getgenv().Key = "MARU-78T3-G54LS-1DH2-AHI95-NI09"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 end)
 task.spawn(function()
-    wait(30)
+    wait(50)
     if not game.CoreGui:FindFirstChild('NINONOOB') then
         game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId)
     end
