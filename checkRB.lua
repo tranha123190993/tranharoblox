e9d2610a3da24a2eb08cfeceb549b597479c071c
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-03 13:05:13 +0700

    Update checkRB.lua

--- checkRB.lua
+++ checkRB.lua
@@ -1,6 +1,22 @@
-task.spawn(function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
+spawn(function()
+local RunService = game:GetService("RunService")
+RunService:Set3dRenderingEnabled(false)
+setfpscap(30)
+getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
+getgenv().id = "513996919622860832"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+end)
+spawn(function()
+    wait(50)
+    if not game.CoreGui:FindFirstChild('NINONOOB') then
+        game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId)
+    end
+end)
+spawn(function()
+        repeat task.wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cangyeunhaudau/ditmemaychominhtriet/main/mankefarm.lua"))()
         end)
-task.spawn(function()
+spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/chimnguu/ngu/master/bululachip.lua"))()
 end)
