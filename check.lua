4072cd8450f2dd186c992a2cff8d96505ffee1cd
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-03 08:44:59 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -7,7 +7,7 @@ getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
 end)
-task.spawn(function()
+spawn(function()
     wait(50)
     if not game.CoreGui:FindFirstChild('NINONOOB') then
         game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId)
