53ec0c78b994e9abe81760d738e6961ce74f05d5
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-04 00:41:50 +0700

    Update robloxcheck.lua

--- robloxcheck.lua
+++ robloxcheck.lua
@@ -1,5 +1,7 @@
 task.spawn(function()
-setfpscap(15)
+                local RunService = game:GetService("RunService")
+RunService:Set3dRenderingEnabled(false)
+setfpscap(30)
 getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
