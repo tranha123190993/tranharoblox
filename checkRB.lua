22e2bf7b8cf359c94bd3f93ede9009c6e8e50984
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-03 15:41:39 +0700

    Update checkRB.lua

--- checkRB.lua
+++ checkRB.lua
@@ -1,7 +1,4 @@
 spawn(function()
-local RunService = game:GetService("RunService")
-RunService:Set3dRenderingEnabled(false)
-setfpscap(30)
 getgenv().Key = "MARU-NC03-TVRRW-7ZFM-KBVH8-WRHR"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
