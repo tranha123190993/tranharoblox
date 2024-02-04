f0e2ca12e9289ff4195d2a641ae109a6a00a1753
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-04 11:05:58 +0700

    Update robloxcheck.lua

--- robloxcheck.lua
+++ robloxcheck.lua
@@ -1,9 +1,7 @@
-task.spawn(function()
 getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-        end)
 task.spawn(function()
         -- Variable to enable frame rate optimization features
 FrameRateBoost = true
