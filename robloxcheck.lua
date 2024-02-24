07d22b4c9b065429fe518ce7ffd7b72b9ad37722
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-02-24 22:58:19 +0700

    Update robloxcheck.lua

--- robloxcheck.lua
+++ robloxcheck.lua
@@ -116,7 +116,7 @@ _G.GunSettings = { -- ปืนที่จะทำ
     ['SoulGuitar'] = true
 }
 _G.SkillC = false --ปิดสกิว C
-getgenv().Key = "MARU-NC03-TVRRW-7ZFM-KBVH8-WRHR"
+getgenv().Key = "MARU-MWYO-MIAEH-KG6R-ZZB6B-YMM7"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
