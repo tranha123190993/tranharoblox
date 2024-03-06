65bf2007636520c71d71eb1c52f91dee0fa69d47
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-03-07 00:38:14 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -99,7 +99,7 @@ task.spawn(function()
         game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId)
     end
 end)
-getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
+getgenv().Key = "MARU-AKL3-MNYWZ-17AR-7G2F2-N3G3"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
