4db6337eef0c03ef92ea83cb0bdf35c827efa99d
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-03-14 00:48:40 +0700

    Update robloxcheck.lua

--- robloxcheck.lua
+++ robloxcheck.lua
@@ -99,7 +99,7 @@ task.spawn(function()
         game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId)
     end
 end)
-getgenv().Key = "MARU-MWYO-MIAEH-KG6R-ZZB6B-YMM7"
+getgenv().Key = "MARU-78T3-G54LS-1DH2-AHI95-NI09"
 getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
