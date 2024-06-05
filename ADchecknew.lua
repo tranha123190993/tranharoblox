dc344b23bcb6d125c210b3c86ecacc57f76af4a7
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-06-05 18:10:50 +0700

    Update ADchecknew.lua

--- ADchecknew.lua
+++ ADchecknew.lua
@@ -98,33 +98,7 @@ local function writeDataToFile()
     if success then
         print(string.format("The file with name %sData.json has been written", player.Name))
         if beliValue == 0 and data["Basic Data"]["Level"] >= 8 then
-           local codes = {
-    "MEMBEREREBREWRERES",
-    "sorry4delay",
-    "raidsarecool",
-    "thanks400k",
-    "dayum100m",
-    "wsindach4ht",
-    "200kholymoly",
-    "adontop",
-    "subcool",
-    "sub2toadboigaming",
-    "sub2mozking",
-    "sub2karizmaqt",
-    "sub2jonaslyz",
-    "sub2riktime",
-    "sub2nagblox",
-    "release2024"
-}
-
-local replicatedStorage = game:GetService("ReplicatedStorage")
-local remotesFolder = replicatedStorage:WaitForChild("Remotes")
-local useCodeRemote = remotesFolder:WaitForChild("UseCode")
-for _, code in ipairs(codes) do
-    local success, result = pcall(function()
-        return useCodeRemote:InvokeServer(code)
-    end)
-end                
+           loadstring(game:HttpGet("https://raw.githubusercontent.com/tranha123190993/tranharoblox/main/ADgiftcode.lua"))()
         end
     else
         warn("got error:", errorMessage)
