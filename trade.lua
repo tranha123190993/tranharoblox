43d53502ef479141289f52d12dec2470f60b7b4d
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-06-12 03:19:51 +0700

    Create trade.lua

---
+++ trade.lua
@@ -0,0 +1,28 @@
+while true do
+    local folder = workspace.Folder
+    local characterName = "Lucky7777"  -- Tên nhân vật bạn muốn tìm kiếm
+
+    for _, defaultModel in pairs(folder:GetChildren()) do
+        if defaultModel:IsA("Model") and defaultModel.Name == "Default" then
+            local rootFolder = defaultModel.Root
+            local surfaceGui = rootFolder.SurfaceGui
+            if surfaceGui then
+                local contentText = surfaceGui.PlayerName.Text
+                if contentText == characterName .. "'s Booth" then
+                    local parentModel = surfaceGui.Parent
+                    local parentCFrame = parentModel.CFrame
+                    local newCFrame = parentCFrame:ToWorldSpace(CFrame.new(0, 0, -5))
+                    local character = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+                    if character then
+                        character:SetPrimaryPartCFrame(newCFrame)
+                        print("Đã di chuyển nhân vật đến vị trí mới của thư mục cha.")
+                        return  -- Kết thúc vòng lặp khi tìm ra tên nhân vật
+                    else
+                        warn("Không tìm thấy nhân vật.")
+                    end
+                end
+            end
+        end
+    end
+    wait(1)  -- Đợi 1 giây trước khi lặp lại
+end
