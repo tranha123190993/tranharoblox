0de904f6a5f721c157fb19187b1dcfb9184d7122
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-06-15 19:22:32 +0700

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -1,7 +1,7 @@
 _G.SettingsAD = {
         ["LeaveAtWave"] = 31,
         ["MethodLeave"] = "Sell", -- Sell , Leave
-        ["ReplayInStoryMode"] = true -- Use when u want to play only story mode ( next time will go to next chapter too when disconnect )
+        ["ReplayInStoryMode"] = false -- Use when u want to play only story mode ( next time will go to next chapter too when disconnect )
     }
 getgenv().Key = "MARU-TIRX-WMBV6-WBES-4AQD4-QRHB"
 getgenv().id = "513996919622860832"
