049018aa53d98d08021ed19037bacd69a88ce62c
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-06-25 02:51:50 +0700

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -1,8 +1,4 @@
-_G.SettingsAD = {
-        ["LeaveAtWave"] = 31,
-        ["MethodLeave"] = "Sell", -- Sell , Leave
-        ["ReplayInStoryMode"] = true -- Use when u want to play only story mode ( next time will go to next chapter too when disconnect )
-    }
+_G.ReplayConfigs = true
 getgenv().Key = "MARU-TOZD-SOM5K-17FA-U4169-A3EP"
 getgenv().id = "1111201035181109318"
 getgenv().Script_Mode = "Kaitun_Script"
