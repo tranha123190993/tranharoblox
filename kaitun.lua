65df565b24ce5c09ebdea9ac2f9a4634afa05a72
tranha123190993 <158168160+tranha123190993@users.noreply.github.com>
2024-06-26 12:51:21 +0700

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -1,5 +1,5 @@
-_G.ReplayConfigs = true
-getgenv().Key = "MARU-TOZD-SOM5K-17FA-U4169-A3EP"
-getgenv().id = "1111201035181109318"
+--_G.ReplayConfigs = true
+getgenv().Key = "MARU-0AJ31-SYEEG-WD28-AQ1ET-509B"
+getgenv().id = "513996919622860832"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
